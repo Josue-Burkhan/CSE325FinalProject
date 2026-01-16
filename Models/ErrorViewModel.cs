c76f0f5e3721d5400d103ff3de6c63fdcb36a36a
Josue Burkhan <josue.burkhan@gmail.com>
2026-01-15 23:06:32 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSE325FinalProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
